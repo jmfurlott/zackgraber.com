fac776f93cd50558d20923eefa8db747128da41e
Joseph Furlott <jmfurlott@gmail.com>
2015-05-03 23:20:00 -0400

    ENH: Added Exo web font

--- wp-content/themes/zackgraber/functions.php
+++ wp-content/themes/zackgraber/functions.php
@@ -101,7 +101,7 @@ add_action( 'widgets_init', 'zackgraber_widgets_init' );
  */
 function zackgraber_scripts() {
 	wp_enqueue_style( 'zackgraber-style', get_stylesheet_uri() );
-
+    wp_enqueue_style( 'exo-web-font', 'http://fonts.googleapis.com/css?family=Exo+2:400,100italic,300italic,300,700,900');
 	wp_enqueue_script( 'zackgraber-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
 
 	wp_enqueue_script( 'zackgraber-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
