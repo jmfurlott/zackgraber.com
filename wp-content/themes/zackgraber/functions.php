9d97af8dbef6391175155d247d28f3704af2f161
Joseph Furlott <jmfurlott@gmail.com>
2015-05-16 12:34:58 -0400

    ENH: Added project cpt. Fixes #1

--- wp-content/themes/zackgraber/functions.php
+++ wp-content/themes/zackgraber/functions.php
@@ -136,3 +136,32 @@ require get_template_directory() . '/inc/customizer.php';
  * Load Jetpack compatibility file.
  */
 require get_template_directory() . '/inc/jetpack.php';
+
+// Create Project cpt
+function cpt_projects() {
+  $labels = array(
+    'name'               => _x( 'Projects', 'post type general name' ),
+    'singular_name'      => _x( 'Project', 'post type singular name' ),
+    'add_new'            => _x( 'Add New', 'book' ),
+    'add_new_item'       => __( 'Add New Project' ),
+    'edit_item'          => __( 'Edit Project' ),
+    'new_item'           => __( 'New Project' ),
+    'all_items'          => __( 'All Projects' ),
+    'view_item'          => __( 'View Project' ),
+    'search_items'       => __( 'Search Projects' ),
+    'not_found'          => __( 'No projects found' ),
+    'not_found_in_trash' => __( 'No projects found in the Trash' ),
+    'parent_item_colon'  => '',
+    'menu_name'          => 'Projects'
+  );
+  $args = array(
+    'labels'        => $labels,
+    'description'   => 'Portfolio projects',
+    'public'        => true,
+    'menu_position' => 5,
+    'supports'      => array( 'title', 'editor', 'thumbnail', 'excerpt', 'comments' ),
+    'has_archive'   => true,
+  );
+  register_post_type( 'project', $args ); 
+}
+add_action( 'init', 'cpt_projects' );
