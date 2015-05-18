ab70af38478150f0a3c8952130649d33de215dd1
Joseph Furlott <jmfurlott@gmail.com>
2015-05-18 10:06:24 -0400

    ENH: Added modules; Headers started; About page started

---
+++ wp-content/themes/zackgraber/page-about.php
@@ -0,0 +1,48 @@
+<?php
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+      <h1>I have a confession.</h1>
+      <h2>I ended up in the design field by accident.</h2>
+
+      <div class="dark-background">
+        <h3>Background</h3>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vel faucibus turpis. Sed tempus, mi eget accumsan consequat, sapien erat gravida odio, ac vulputate lorem ex euismod arcu. Sed nulla enim, eleifend eu eros sed, tempus eleifend augue. In efficitur est ac rhoncus sollicitudin. Suspendisse nec orci a orci ultrices mattis. Nunc egestas massa a neque porttitor, a viverra purus venenatis. Integer tempus consequat congue. Morbi at consequat arcu, ac gravida elit. Vestibulum sit amet sagittis nunc. Praesent cursus enim quam.
+        </p>
+        <p>
+            Suspendisse blandit fermentum finibus. Duis in sollicitudin nisi. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Fusce aliquam porttitor justo, quis cursus sem pellentesque eget. Quisque urna arcu, feugiat vel elit nec, ullamcorper placerat mi. Mauris sit amet sagittis felis. Donec ornare elit libero, feugiat ultricies risus aliquet sed. Donec sodales maximus leo, id fermentum magna ullamcorper ac. Nullam feugiat ante vel libero bibendum, vitae lobortis diam dignissim. Sed fermentum vel massa et varius. In hac habitasse platea dictumst. Cras eget leo enim. Curabitur volutpat tristique est vel efficitur. Curabitur justo leo, accumsan at sodales sit amet, imperdiet ac velit. Interdum et malesuada fames ac ante ipsum primis in faucibus.
+        </p>
+      </div>
+
+      <div class="light-background">
+        <h3>Philosophy</h3>
+        <p>
+          Ut dictum tellus vel purus ullamcorper, at iaculis velit finibus. Pellentesque tempor nisi libero, vel placerat justo malesuada non. Vivamus ac ante ac elit volutpat pharetra. Proin imperdiet ullamcorper elit semper pretium. Sed mollis nisl ac massa pellentesque, a aliquam neque malesuada. Aliquam elementum, urna quis commodo sodales, sapien est convallis nisi, scelerisque pulvinar lacus sapien a erat. Suspendisse sit amet orci id arcu fringilla fermentum vitae nec nulla. Sed dignissim ipsum a tortor hendrerit porta.
+        </p>
+        <p>
+          Morbi ut tristique nibh. Aliquam pulvinar tristique risus.
+        </p>
+      </div>
+
+      <div class="dark-background">
+        <h3>Capabilities</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor nisi eget turpis malesuada tempor. Vestibulum congue id nisl at porta. Fusce non sagittis lorem, in tempor augue. Mauris vitae tortor et dui imperdiet consequat. Curabitur at justo risus. Cras pulvinar metus ut odio mattis fermentum. Aenean lobortis nibh ut dolor congue blandit. Quisque eu magna sit amet erat placerat aliquet. Mauris pellentesque ligula quis nibh tristique tincidunt. Quisque dolor mauris, placerat eu quam vitae, pulvinar dapibus arcu. Donec quis vulputate nibh. Morbi gravida purus non purus scelerisque egestas.</p>
+      </div>
+
+      <div class="light-background">
+        <h3>Dream Projects</h3>
+        <p>I'm psyched about every project I work on, but there are few unicrns I'm try to catch.  If you're one of these, for the love of Rand, give me a call!</p>
+      </div>
+
+      <div class="dark-background">
+        <h3>Obligatory Photo of Me</h3>
+        <p>This is me standing in front of a class of design students at Santa Fe College. I was talking to them about how to get a job after they graduate.  Public speaking is terrifying for me but it's really rewarding. If you'd like me to talk to anyone about what I do, give me a shout.</p>
+      </div>
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
