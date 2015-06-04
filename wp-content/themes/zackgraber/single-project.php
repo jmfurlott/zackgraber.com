ce2e316e4f8a4556016360ab9e73882a5568546f
Joseph Furlott <jmfurlott@gmail.com>
2015-06-04 00:10:42 -0400

    ENH: Added all the content from a single project

--- wp-content/themes/zackgraber/single-project.php
+++ wp-content/themes/zackgraber/single-project.php
@@ -10,6 +10,7 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 		  <?php while ( have_posts() ) : the_post(); ?>
+        <!-- Titles -->
         <div class="container">
           <h1><?php the_title(); ?></h1>
           <h2><?php the_field("subtitle"); ?></h2>
@@ -21,11 +22,34 @@ get_header(); ?>
             </div>
         </div>
 
+        <!-- Before and after images -->
         <?php $before_image = get_field("before_picture"); ?>
         <?php $after_image = get_field("after_picture"); ?>
         <div class="brands">
            <img src="<?php echo $before_image['url']; ?>">
            <img src="<?php echo $after_image['url']; ?>">
+        </div>
+
+        <!-- Infinite pictures -->
+        <div class="images">
+        <?php
+        if(have_rows("images")) {
+          while(have_rows("images")) {
+            the_row();
+            $image = get_sub_field("image");
+          ?>
+            <div class="image">
+                <img src="<?php echo $image['url']; ?>">
+            </div>
+          <?php
+          }
+        }
+        ?>
+        </div>
+
+        <!-- Extra content -->
+        <div class="container">
+            <?php the_field("last_content"); ?>
         </div>
 		<?php endwhile; // end of the loop. ?>
 
