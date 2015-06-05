a03539aabb1072ea6a78e84b20bcc1d91bed4a27
Joseph Furlott <jmfurlott@gmail.com>
2015-06-05 19:51:27 -0400

    ENH: Properly showing bg

--- wp-content/themes/zackgraber/index.php
+++ wp-content/themes/zackgraber/index.php
@@ -13,7 +13,7 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
+	<div id="primary" class="content-area index" style="background-image: url('<?php echo get_stylesheet_directory_uri() ?>/img/background.svg')">
 		<main id="main" class="site-main" role="main">
       <div class="container">
             <div class="index-copy">
