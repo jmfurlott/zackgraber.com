a264ecedd7d7d4e8f65beed5912a2b7320739c6a
Joseph Furlott <jmfurlott@gmail.com>
2015-07-06 18:58:13 -0400

    Edited font size

--- wp-content/themes/zackgraber/header.php
+++ wp-content/themes/zackgraber/header.php
@@ -13,11 +13,21 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-
 <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
+
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+     (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+       m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+     })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-64023411-1', 'auto');
+  ga('send', 'pageview');
+</script>
+
 <div id="page" class="hfeed site">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'zackgraber' ); ?></a>
   <div class="container">
